a1628b6e71ca75a402b62746247c62475dc17c63
Salmin Skenderovic <salmin.skenderovic@resurs.se>
2017-09-26 11:10:48 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="">
+            <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" />
+            Sample
+          </a>
+        </nav>
       </div>
     );
   }
