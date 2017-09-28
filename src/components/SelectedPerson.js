2c5b4fba0e3d20b30dc60f07c695fb7fa0ba8aa6
Salmin Skenderovic <salmin.skenderovic@resurs.se>
2017-09-28 11:22:01 +0200

    created a problem

--- src/components/SelectedPerson.js
+++ src/components/SelectedPerson.js
@@ -3,21 +3,79 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 // Component
-import PeopleList from '../components/PeopleList';
 
 class SelectedPerson extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedPerson: {
+                name: "",
+                age: "",
+                city: ""
+            }
+        };
+
+        // this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentWillReceiveProps(prop) {
+        const { selectedPerson } = this.props.personStore;
+        if (selectedPerson) {
+            this.setState({selectedPerson: selectedPerson})
+        }
+    }
+
+    savePerson(e) {
+        e.preventDefault();
+
+        const updatedPerson = {
+            name: this.refs.name.value,
+            age: this.refs.age.value,
+            city: this.refs.city.value,
+            children: this.props.personStore.selectedPerson.children
+        }
+        console.log(updatedPerson);
+    }
+
+    handleChange(e) {
+        console.log(e.target.value);
+        // debugger; show
+        // this.setState({value: e.target.value});
+    }
 
     render() {
 
         const { selectedPerson } = this.props.personStore;
         return (
             <div>
+                {JSON.stringify(selectedPerson)}
                 {selectedPerson && <div className="row">
-                    <div className="col-md-12"><h2>Selected person</h2></div>
-                    <div className="col-md-12">{selectedPerson.name}</div>
-                    <div className="col-md-12">{selectedPerson.age}</div>
-                    <div className="col-md-12">{selectedPerson.city}</div>
-                    { selectedPerson.children.length > 0 &&  <PeopleList people={selectedPerson.children} />} 
+                    <form onSubmit={(e) => this.savePerson(e)}>
+                        <div className="col-md-12"><h2>Selected person</h2></div>
+
+                        <div className="form-group">
+                            <label>Name:</label>
+                            <input type="text" ref="name" className="form-control" value={this.state.selectedPerson.name} onChange={ this.handleChange } />
+                        </div>
+
+                        <div className="form-group">
+                            <label>Age:</label>
+                            <input type="number" ref="age" className="form-control" defaultValue={selectedPerson.age} />
+                        </div>
+
+
+                        <div className="form-group">
+                            <label>City:</label>
+                            <input type="text" ref="city" className="form-control" defaultValue={selectedPerson.city} />
+                        </div>
+
+
+                        <div className="form-group">
+                            <button className="btn btn-default" style={{ cursor: 'pointer' }}>Save</button>
+                        </div>
+                        
+                    </form>
                 </div>}
             </div>
         )
